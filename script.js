4ebab23cb100f3599db1a3c7962e0c17b84f2a2c
Adriane-lSilva <122410076+Adriane-lSilva@users.noreply.github.com>
2023-01-18 22:12:42 -0300

    Feito

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+var meses=["Janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+
+var tela;
+for (tela=0;
+ tela<meses.length;
+ 
+ tela++)
+   {  
+       alert(meses[tela]);
+    }
+ 
+
+
